03e958a25309ac0f38f59a24a1062c6171aed3c2
Enzo Weder <enzowederma@gmail.com>
2022-10-02 22:37:26 -0300

    ✨ feat: add user information into card container

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import './App.css'
 
 function App() {
+  const userName = "Enzo Weder";
+  const userMajor = "Sistemas de Informação";
+  const userPhone = "(85) 99421-6158";
+  const userEmail = "enzoweder@ufu.br"
+  const userURL = "www.ufu.com.br"
+  const userLocation = "UFU - Monte Carmelo - Unidade Araras"
 
   return (
-    <div>
-      <button>Aluno</button>
-      <button>Professor</button>
-      <button>Outro</button>
-    </div>
+    <>
+      <div className="container" id='user-type' >
+        <button>Aluno</button>
+        <button>Professor</button>
+        <button>Outro</button>
+        <div className='container' id='card'>
+          <h4>Universidade Federal de</h4>
+          <h2>Uberlândia</h2>
+          <h1>{userName}</h1>
+          <h2>Aluno de {userMajor}</h2>
+          <table>
+            <tr>{userEmail}</tr>
+            <tr>{userPhone}</tr>
+            <tr>{userURL}</tr>
+            <tr>{userLocation}</tr>
+          </table>
+        </div>
+      </div>
+    </>
+
   )
 }
 
