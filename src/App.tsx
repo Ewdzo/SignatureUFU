078b9d258a49c881f6c82fc2f77a23761021d96d
Enzo Weder <enzowederma@gmail.com>
2022-10-04 23:30:24 -0300

    🦄 refactor: change card parent

--- src/App.tsx
+++ src/App.tsx
@@ -15,24 +15,25 @@ function App() {
         <button>Aluno</button>
         <button>Professor</button>
         <button>Outro</button>
-        <div className='container' id='card'>
-
-          <table>
-            <h4>Universidade Federal de</h4>
-            <h2>Uberlândia</h2>
-          </table>
-          
-
-          <table id='userInfo'>
-            <tr><h1>{userName}</h1></tr>
-            <tr><h2>Alun{userPronouns} de {userMajor}</h2></tr>
-            <tr><img src="src\imgs\mail_icon.png" alt="Mail Icon" />{userEmail}</tr>
-            <tr><img src="src\imgs\phone_icon.png" alt="Phone Icon" />{userPhone}</tr>
-            <tr><img src="src\imgs\person_icon.png" alt="Person Icon" />{userURL}</tr>
-            <tr><img src="src\imgs\location_icon.png" alt="Location Icon" />{userLocation}</tr>
-          </table>
-        </div>
       </div>
+      <div className='container' id='card'>
+
+        <table>
+          <h4>Universidade Federal de</h4>
+          <h2>Uberlândia</h2>
+        </table>
+        
+
+        <table id='userInfo'>
+          <tr><h1>{userName}</h1></tr>
+          <tr><h2>Alun{userPronouns} de {userMajor}</h2></tr>
+          <tr><img src="src\imgs\mail_icon.png" alt="Mail Icon" />{userEmail}</tr>
+          <tr><img src="src\imgs\phone_icon.png" alt="Phone Icon" />{userPhone}</tr>
+          <tr><img src="src\imgs\person_icon.png" alt="Person Icon" />{userURL}</tr>
+          <tr><img src="src\imgs\location_icon.png" alt="Location Icon" />{userLocation}</tr>
+        </table>
+      </div>
+
     </>
 
   )
