6cb4cfdaffb9967e8e691b12aeb9ad99211f2f13
Enzo Weder <enzowederma@gmail.com>
2022-10-31 18:24:40 -0300

    ✨ feat: converts info-input class to styled component

--- src/components/usertype/index.tsx
+++ src/components/usertype/index.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
-const InfoInputContainer = styled.div`
+
+export const InfoInput = styled.div`
 min-height: 500px;
 width: 80%;
 
@@ -14,14 +15,3 @@ border-radius: 2%;
 
 position: relative;
 `
-
-export function TypePicker() {
-
-    return (
-        <InfoInputContainer id='user-type-picker' >
-            <div className="info-card"><input type="radio" name="user-type" id="student-teacher-type" value="student-teacher" /><label htmlFor="student-teacher-type"><img src="./src/imgs/student-fill.svg" alt="" /></label></div>
-            <div className="info-card"><input type="radio" name="user-type" id="other-type" value="other" /><label htmlFor="other-type"><img src="./src/imgs/user-gear-fill.svg" alt="" /></label></div>
-            <div id='page-button'><button id='previous-page'><img src="./src/imgs/arrow-circle-left-fill.svg" alt="" /></button><button id='next-page'><img src="./src/imgs/arrow-circle-right-fill.svg" alt="" /></button></div>
-        </InfoInputContainer>
-    )
-}
