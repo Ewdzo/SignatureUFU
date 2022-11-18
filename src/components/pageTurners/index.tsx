35074259e88ca050b899e80c5c10a9d3ca78bb66
Enzo Weder <enzowederma@gmail.com>
2022-11-17 23:02:15 -0300

    ✨ feat: new PageTurner styled components

---
+++ src/components/pageTurners/index.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+export const PageTurnerIcon = styled.img`
+height: 35px;
+`
+
+export const PageTurnerButton = styled.button`
+border: 0;
+background: none;
+`
