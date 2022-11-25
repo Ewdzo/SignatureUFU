60f10e7ec3e6014c5129d07b980e77daeab08b65
Enzo Weder <enzowederma@gmail.com>
2022-11-24 22:35:28 -0300

    ✨ feat: navigation via button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import 'bootstrap'
 import axios from 'axios';
 import html2canvas from 'html2canvas';
 import QrScanner from 'qr-scanner';
@@ -6,19 +7,18 @@ import images from './imgs/index.jsx';
 import { useEffect, useState } from 'react';
 import { Card } from './components/card';
 import { InfoRadioButton } from './components/infoRadioButton';
-import { PageTurnerButton, PageTurnerIcon } from './components/pageTurners';
 import { InfoCard, InfoCardImage, InfoInputContainer } from './components/infoCard';
 
 function App() {
-  const [userName, setUserName] = useState(String);
-  const [userType, setUserType] = useState(String);
-  const [userPronouns, setUserPronouns] = useState(String);
-  const [userMajor, setUserMajor] = useState(String);
-  const [userPhone, setUserPhone] = useState(String);
-  const [userEmail, setUserEmail] = useState(String);
-  const [userURL, setUserURL] = useState("www.ufu.com.br");
-  const [userLocation, setUserLocation] = useState(String);
-  const [cardTheme, setCardTheme] = useState("blue" as string);
+  const [userName, setUserName] = useState<String>();
+  const [userType, setUserType] = useState<String>();
+  const [userPronouns, setUserPronouns] = useState<String>();
+  const [userMajor, setUserMajor] = useState<String>();
+  const [userPhone, setUserPhone] = useState<String>();
+  const [userEmail, setUserEmail] = useState<String>();
+  const [userURL, setUserURL] = useState<String>("www.ufu.com.br");
+  const [userLocation, setUserLocation] = useState<String>();
+  const [cardTheme, setCardTheme] = useState<String>("blue");
 
   const printCard = async () => {
 
@@ -67,7 +67,22 @@ function App() {
     .catch(e => console.log('No QR code found.'));
   };
 
+  const scrollTo = (index: number) => {
+    const navButtons = document.getElementsByClassName("nav-button");
+    for(let i=0; i< navButtons.length; i++){
+      (navButtons[i] as HTMLElement).classList.remove("selected");
+    }
+
+    (navButtons[index] as HTMLElement).classList.add("selected");
+
+    const infoContainers = document.querySelectorAll(".infoContainer");
+
+    (infoContainers[index] as HTMLElement).scrollIntoView({behavior: 'smooth'})
+  }
+
   useEffect(() => {
+    const navButtons = document.querySelectorAll(".nav-button");
+    navButtons.forEach( (element, index) => {(element as HTMLElement).onclick = () => { scrollTo(index)}});
 
     const qrInput = document.getElementById("qr-code-submit") as HTMLInputElement;
     qrInput.onchange = () => {  
@@ -79,14 +94,12 @@ function App() {
     const printButton = document.getElementById("print-button");
     (printButton as HTMLInputElement).onclick = () => { printCard() };
 
-    const userTypeButtons = document.getElementsByName("user-type");
     const TeacherFacultyInput = (document.querySelector('input[name="teacher-faculty"]') as HTMLInputElement);
     const userPronounsInput = document.querySelectorAll('input[name="user-gender"]');
     const userEmailInput = (document.querySelector('input[name="user-email"]') as HTMLInputElement);
     const userPhoneInput = (document.querySelector('input[name="user-phone"]') as HTMLInputElement);
     const userLocationInput = document.querySelectorAll('input[name="user-location"]');
     const teacherRoomInput = (document.querySelector('input[name="teacher-room"]') as HTMLInputElement);
-
 
     TeacherFacultyInput.onchange = () => { 
       if(userType == "Professor") {setUserMajor(((document.querySelector('input[name="teacher-faculty"]') as HTMLInputElement).value) as unknown as string)};
@@ -122,19 +135,31 @@ function App() {
 
   return (
     <>
+      <div id='nav-buttons-container'>
+        <ul>
+          <li><a className='nav-button'></a></li>
+          <li><a className='nav-button'></a></li>
+          <li><a className='nav-button'></a></li>
+          <li><a className='nav-button'></a></li>
+          <li><a className='nav-button'></a></li>
+          <li><a className='nav-button'></a></li>
+          <li><a className='nav-button'></a></li>
+          <li><a className='nav-button'></a></li>
+          <li><a className='nav-button'></a></li>
+          <li><a className='nav-button'></a></li>
+        </ul>        
+      </div>
       <div id='main-container'>
-      
-        <div id='nav-buttons-container'><a href="user-type-picker">AAAA</a></div>
-        <InfoInputContainer id='user-type-picker' >
+        <InfoInputContainer className="infoContainer" id='user-type-picker' >
             <InfoCard><InfoRadioButton name="user-type" id="student-teacher-type" value="student-teacher" /><label htmlFor="student-teacher-type"><InfoCardImage src={images[0]} alt="" /></label></InfoCard>
             <InfoCard><InfoRadioButton name="user-type" id="other-type" value="other" /><label htmlFor="other-type"><InfoCardImage src={images[1]} alt="" /></label></InfoCard>
         </InfoInputContainer>
 
-        <InfoInputContainer id='user-qr-submitter'>
+        <InfoInputContainer className="infoContainer" id='user-qr-submitter'>
           <input type="file" id="qr-code-submit" name="" />
         </InfoInputContainer>
 
-        <InfoInputContainer>
+        <InfoInputContainer className="infoContainer" id="user-information-submitter">
           <div className='info-type-container'>
             <InfoCardImage src={images[10]} alt="" />
             <input type="text" name="user-name" id="user-name" placeholder='Ex: Enzo Weder' />
@@ -144,36 +169,36 @@ function App() {
           </div>         
         </InfoInputContainer>
         
-        <InfoInputContainer id='teacher-faculty-container'>
+        <InfoInputContainer className="infoContainer" id='teacher-faculty-container'>
           <div className='info-type-container'><InfoCardImage src={images[4]} alt="" /><input type="text" name="teacher-faculty" id="teacher-faculty" placeholder='Ex: FACOM' /></div>          
         </InfoInputContainer>
 
-        <InfoInputContainer id='user-pronouns-picker'>
+        <InfoInputContainer className="infoContainer" id='user-pronouns-picker'>
           <InfoCard><InfoRadioButton name="user-gender" id="male-gender" value="o" /><label htmlFor="male-gender"><InfoCardImage src={images[5]} /></label></InfoCard>
           <InfoCard><InfoRadioButton name="user-gender" id="female-gender" value="a" /><label htmlFor="female-gender"><InfoCardImage src={images[6]} alt="" /></label></InfoCard>
           <InfoCard><InfoRadioButton name="user-gender" id="other-gender" value="e" /><label htmlFor="other-gender"><InfoCardImage src={images[7]} alt="" /></label></InfoCard>           
         </InfoInputContainer>
 
-        <InfoInputContainer id='user-contact-picker'>
+        <InfoInputContainer className="infoContainer" id='user-contact-picker'>
           <InfoCard><InfoCardImage src={images[8]} alt="" /><input type="email" name="user-email" id="user-email" placeholder='Ex: aluno@ufu.br' /></InfoCard>
           <InfoCard><InfoCardImage src={images[9]} alt="" /><input type="tel" name="user-phone" id="user-phone" placeholder='Ex: (34) 3810-1010' /></InfoCard>          
         </InfoInputContainer>
         
-        <InfoInputContainer id='user-location-container'>
+        <InfoInputContainer className="infoContainer" id='user-location-container'>
           <InfoCard><InfoRadioButton name="user-location" id="araras-campus" value="Monte Carmelo - Unidades Araras" /><label htmlFor="araras-campus">Monte Carmelo - Araras</label></InfoCard>
           <InfoCard><InfoRadioButton name="user-location" id="boa-vista-campus" value="Monte Carmelo - Unidades Boa Vista" /><label htmlFor="boa-vista-campus">Monte Carmelo - Boa Vista</label></InfoCard>        
         </InfoInputContainer>
 
-        <InfoInputContainer id='teacher-location-container'>
+        <InfoInputContainer className="infoContainer" id='teacher-location-container'>
           <InfoCard><input type="text" name="teacher-room" placeholder='Ex: A201' /></InfoCard>
         </InfoInputContainer>
 
-        <InfoInputContainer id='card-theme-picker'>
+        <InfoInputContainer className="infoContainer" id='card-theme-picker'>
           <InfoCard><InfoRadioButton name="card-theme" id="blue-theme" value="blue" /><label htmlFor="blue-theme">Blue</label></InfoCard>
           <InfoCard><InfoRadioButton name="card-theme" id="white-theme" value="white" /><label htmlFor="white-theme">White</label></InfoCard>     
         </InfoInputContainer>
 
-        <InfoInputContainer id='print-container'>
+        <InfoInputContainer className="infoContainer" id='print-container'>
           <input type="button" value="print" id="print-button" />
         </InfoInputContainer>   
       </div>
