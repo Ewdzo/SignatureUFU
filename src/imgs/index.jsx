60b97849565795ac832bdbebf568a5f42836e5a0
Enzo Weder <enzowederma@gmail.com>
2022-11-28 18:44:04 -0300

    ↩ revert: un-delete person icon

--- src/imgs/index.jsx
+++ src/imgs/index.jsx
@@ -8,8 +8,9 @@ import GenderFemale from "./gender-female.svg";
 import GenderNonBinary from "./gender-non-binary.svg";
 import Envelope from "./envelope-fill.svg";
 import WhatsApp from "./whatsapp-logo-fill.svg";
+import Person from "./person_icon.png";
 import Pencil from "./pencil-fill.svg";
 
-const images = [Student, UserGear, CircleLeft, CircleRight, IdentificationCard, GenderMale, GenderFemale, GenderNonBinary, Envelope, WhatsApp, Pencil];
+const images = [Student, UserGear, CircleLeft, CircleRight, IdentificationCard, GenderMale, GenderFemale, GenderNonBinary, Envelope, WhatsApp, Person, Pencil];
 
 export default images;
