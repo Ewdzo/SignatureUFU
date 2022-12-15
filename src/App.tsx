a4ee21100b93d508cbfb6649f18198aa9528384c
Enzo Weder <enzowederma@gmail.com>
2022-12-14 23:31:55 -0300

    🎈 perf: add error show for qrInput
    If image selected in input file doesn't contain a valid IDUFU Qr, it will be displayed

--- src/App.tsx
+++ src/App.tsx
@@ -74,13 +74,12 @@ function App() {
         const fullName = response.nome.split(" ");
         const nameSurname = fullName[0] + " " + fullName[1];
         setUserName(nameSurname);  
+        (document.getElementById("qr-feedback") as HTMLSpanElement).innerText = `Olá, ${nameSurname}`
 
       })
-      .catch(function (error) {
-        console.log(error);
-      })
+      .catch( e => (document.getElementById("qr-feedback") as HTMLSpanElement).innerText = 'Não conseguimos encontrar um QR válido na ultima imagem.')
     )
-    .catch(e => console.log('No QR code found.'));
+    .catch( e => (document.getElementById("qr-feedback") as HTMLSpanElement).innerText = 'Não conseguimos encontrar um QR válido na ultima imagem.' );
   };
 
   const scrollTo = (index: number) => {
@@ -230,6 +229,7 @@ function App() {
           <InfoCard>
             <InfoCardImage src={images[17]} alt="Ícone de QrCode" title='QrCode'/>
               <input type="file" id="qr-code-submit" accept=".png, .jpg, .jpeg" style={{margin: "5%"}} />
+              <span id='qr-feedback'></span>
             </InfoCard>
         </InfoInputContainer>
 
